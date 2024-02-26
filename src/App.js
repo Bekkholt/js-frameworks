295dee641213142f3dd8bbf941bf5e2225c8d45b
Tina <tina.bekkholt@hotmail.com>
2024-02-26 13:42:41 +0100

    first attempt header and navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav className="App-nav">
+          <li>Home</li>
+          <li>Contact</li>
+        </nav>
       </header>
     </div>
   );
