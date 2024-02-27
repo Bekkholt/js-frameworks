dd019e686ba0996d2e480ceaaf5bf1bf021bbdd9
Tina <tina.bekkholt@hotmail.com>
2024-02-27 16:02:11 +0100

    fix after merge

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -14,21 +14,3 @@ export default Counter;
 {
   /* <button onClick={onButtonClick}>+</button> */
 }
-
-import { useState } from "react";
-
-function Counter() {
-  const [count, setCount] = useState(0);
-
-  function onButtonClick() {
-    setCount(count + 1);
-  }
-
-  return <div>{count}</div>;
-}
-
-export default Counter;
-
-{
-  /* <button onClick={onButtonClick}>+</button> */
-}
