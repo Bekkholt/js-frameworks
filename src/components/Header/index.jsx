39bcdc32376ba895c8176b97f4ff37298969c287
Tina <tina.bekkholt@hotmail.com>
2024-03-05 11:37:45 +0100

    Created searchbar

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,6 +2,7 @@ import "./index.css";
 import IonCartOutline from "../CartIcon";
 import Counter from "../Counter";
 import { Link } from "react-router-dom";
+import Searchbar from "../Searchbar";
 
 export default function Header() {
   return (
@@ -14,6 +15,7 @@ export default function Header() {
           <Link to="/src/components/ContactPage">Contact</Link>
         </li>
       </nav>
+      <Searchbar />
       <div className="Cart-Icon">
         <Link to="/src/components/CheckoutPage">
           <IonCartOutline />
