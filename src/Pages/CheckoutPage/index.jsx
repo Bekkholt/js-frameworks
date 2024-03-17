11c696c4e6329beb9347ec677a06396d27484fe4
Tina <tina.bekkholt@hotmail.com>
2024-03-17 11:28:29 +0100

    cleanup

--- src/Pages/CheckoutPage/index.jsx
+++ src/Pages/CheckoutPage/index.jsx
@@ -7,14 +7,6 @@ export default function CheckoutPage(inCart) {
     <div>
       <h1>In cart</h1>
       {inCart.inCart.map((inCart, i) => (
-        // <p
-        //   key={i + inCart.id}
-        //   id={inCart.id}
-        //   image={inCart.image}
-        //   title={inCart.title}
-        //   price={inCart.price}
-        //   discountedPrice={inCart.discountedPrice}
-        // />
         <p key={i + inCart.id}>{inCart.title}</p>
       ))}
       <Link to="../../Pages/CheckoutSuccessPage">
