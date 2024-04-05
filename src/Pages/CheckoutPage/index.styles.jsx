07f7aeccfb8f1b4b7f045fb7628c56ea2499c03f
Tina <tina.bekkholt@hotmail.com>
2024-04-05 12:15:26 +0200

    border-color

--- src/Pages/CheckoutPage/index.styles.jsx
+++ src/Pages/CheckoutPage/index.styles.jsx
@@ -4,6 +4,7 @@ export const CheckoutButton = styled.button`
   color: black;
   border: 2;
   background: none;
+  border-color: black;
   background-color: #97b0de;
   border-radius: 0.2rem;
   margin: 2%;
--- src/Pages/CheckoutSuccessPage/index.styles.jsx
+++ src/Pages/CheckoutSuccessPage/index.styles.jsx
@@ -4,6 +4,7 @@ export const BackButton = styled.button`
   color: black;
   border: 2;
   background: none;
+  border-color: black;
   background-color: #97b0de;
   border-radius: 0.2rem;
   padding: 2%;
