3055df8dc0049c41df20cf5b37c4e5897a5771c1
Tina <tina.bekkholt@hotmail.com>
2024-04-06 11:16:35 +0200

    h1 added to checkout success

--- src/Pages/CheckoutSuccessPage/index.jsx
+++ src/Pages/CheckoutSuccessPage/index.jsx
@@ -4,6 +4,7 @@ import * as S from "./index.styles";
 export default function CheckoutSuccessPage() {
   return (
     <div>
+      <h1 className="title">Shop-drop</h1>
       <p className="text">Your order was successful!</p>
       <Link to="/">
         <S.BackButton>Go back</S.BackButton>
